da4125897018dd931e6eb2e70c2c4c9c0415192b
Runek00 <runek00@gmail.com>
2024-01-20 21:55:03 +0100

    I think I'm getting used to the keyboard

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,29 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set('n', '<leader>tt', ':Neotree filesystem toggle right reveal<CR>')
+		local ntree = require("neo-tree")
+		ntree.setup({
+			close_if_last_window = true,
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end
+				},
+        		},
+			window = {
+				mappings = {
+					["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true }},
+				}
+			}
+		})
+	end
+}

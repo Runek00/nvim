ddca161cc9e52ba27aeb0dd193a8ed5a40f148b1
Runek00 <runek00@gmail.com>
2024-01-31 14:16:38 +0100

    debug stop

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -28,5 +28,6 @@ return {
 		vim.keymap.set("n", "<F8>", dap.step_over, {})
 		vim.keymap.set("n", "<F7>", dap.step_into, {})
 		vim.keymap.set("n", "<F6>", dap.step_out, {})
+		vim.keymap.set("n", "<F4>", dap.close, {})
 	end,
 }
